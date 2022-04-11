8515cc6b36d4e5d5e4527d9941624d46449f2787
Diego Cipa <diego@7WK126-1203TR.group.pirelli.com>
2022-04-11 11:33:45 +0200

    Added game win logic

--- src/feature/home/page/game/Game.tsx
+++ src/feature/home/page/game/Game.tsx
@@ -1,9 +1,10 @@
 import styles from './Game.module.scss';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { client } from '../../api/AmiiboClient';
 import { ISeries } from '../../model/ISeries';
 import PlayCard from '../../component/play-card/PlayCard';
+import { shuffleArray } from '../../../../utils/ArrayUtis';
 
 interface GameState {
     key: string;
@@ -15,39 +16,96 @@ interface ICard extends ISeries {
 }
 
 const Game = () => {
-    const [allCharacters, setAllCharacters] = useState<ICard[]>([]);
+    const [allCharacters, setAllCharacters] = useState<ISeries[]>([]);
+    const [gameCharacters, setGameCharacters] = useState<ICard[]>([]);
+    const [lastSelection, setLastSelection] = useState<number | null>(null);
+    const [solvedCharacters, setSolvedCharacters] = useState<number[]>([]);
+    const [wins, setWins] = useState<number>(0);
+    const acceptNewClick = useRef<boolean>(true);
+
     const {state} = useLocation();
     const navigate = useNavigate();
     const seriesName = (state as GameState)?.series;
     const seriesKey = (state as GameState)?.key;
 
     useEffect(() => {
+        acceptNewClick.current = true;
+    }, [lastSelection]);
+
+    useEffect(() => {
+        if (solvedCharacters.length > 0 &&
+            solvedCharacters.length === gameCharacters.length) {
+            restart();
+        }
+    }, [solvedCharacters, gameCharacters]);
+
+    useEffect(() => {
+        const numbers: number[] = Array.from(Array(allCharacters.length).keys());
+        const randomNumbers: number[] = numbers.sort(() => Math.random() - 0.5).slice(0, 5);
+        const chosenCharacters = randomNumbers.map(n => ({
+            ...allCharacters[n],
+            rotate: false
+        }));
+        setGameCharacters(shuffleArray([...chosenCharacters, ...chosenCharacters]));
+    }, [allCharacters, wins]);
+
+    useEffect(() => {
         if (!seriesName || !seriesKey) {
             return navigate('/home');
         }
         const loadRandomCharacters = async (): Promise<void> => {
-            const allSeries = await client.getSeriesByKey(seriesKey).then(res => res.amiibo);
-            const numbers: number[] = Array.from(Array(allSeries.length).keys());
-            const randomNumbers: number[] = numbers.sort(() => Math.random() - 0.5).slice(0, 5);
-            const chosenCharacters = randomNumbers.map(n => ({
-                ...allSeries[n],
-                rotate: false
-            }));
-            setAllCharacters(shuffleArray([...chosenCharacters, ...chosenCharacters]));
+            const allSeries: ISeries[] = await client.getSeriesByKey(seriesKey).then(res => res.amiibo);
+            setAllCharacters(allSeries);
         }
         loadRandomCharacters();
-    }, []);
+    }, [seriesName, seriesKey]);
 
-    const onCardClick = (i: number) => {
-        setAllCharacters(old => old.map((card, index) => {
-            return i === index ? {...card, rotate: !card.rotate} : card;
-        }))
+    const onCardClick = (i: number): void => {
+        if (i === lastSelection || solvedCharacters.includes(i) || !acceptNewClick.current) return;
+        acceptNewClick.current = false;
+        openCard(i);
+        setTimeout(() => {
+            // wait for animation
+            console.log('last selection ' + lastSelection)
+            lastSelection !== null ? handleSecondSelection(i) : setLastSelection(i);
+        }, 500);
+    }
+
+    const openCard = (i: number): void => {
+        setGameCharacters(old => old.map((card, index) => {
+            return i === index ? {...card, rotate: true} : card;
+        }));
+    }
+
+    const handleSecondSelection = (i: number): void => {
+        if (lastSelection == null) {
+            alert('contact police this should not have happened');
+        }
+        const selected = gameCharacters[i];
+        const previous = gameCharacters[lastSelection!];
+        if (selected.name !== previous.name) {
+            setGameCharacters(old => old.map((card, index) => {
+                return (index === i || index === lastSelection) ? {...card, rotate: false} : card;
+            }));
+        }
+
+        if (selected.name === previous.name) {
+            setSolvedCharacters(old => [...old, i, lastSelection!]);
+        }
+        setLastSelection(null);
+    }
+
+    const restart = () => {
+        alert(`Win ${wins + 1}`);
+        setLastSelection(null);
+        setSolvedCharacters([]);
+        setWins(old => old + 1);
     }
 
     return (
         <div className={styles.container}>
             {
-                allCharacters.map((char, i) => (
+                gameCharacters.map((char, i) => (
                     <PlayCard index={i} click={() => onCardClick(i)} key={i} background={char.image}
                               rotate={char.rotate}/>
                 ))
@@ -57,11 +115,3 @@ const Game = () => {
 }
 
 export default Game;
-
-function shuffleArray<T>(array: T[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
